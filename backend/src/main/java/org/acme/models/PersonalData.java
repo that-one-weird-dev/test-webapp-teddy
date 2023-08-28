57dcca5ffc02acc54ec500ef5a7aae71a181981b
manu <manuelugoccioni@gmail.com>
2023-08-28 15:58:08 +0200

    Firstname and surname capitalization

--- backend/src/main/java/org/acme/models/PersonalData.java
+++ backend/src/main/java/org/acme/models/PersonalData.java
@@ -6,12 +6,24 @@ import java.sql.SQLException;
 public record PersonalData(Long id, String firstname, String surname, String email, String address, String place,
                            String city, String province, String notes) {
 
+    @Override
+    public String firstname() {
+        return Character.toUpperCase(firstname.charAt(0)) + firstname.substring(1).toLowerCase();
+    }
+
+    @Override
+    public String surname() {
+        return Character.toUpperCase(surname.charAt(0)) + surname.substring(1).toLowerCase();
+    }
+
     public boolean isValid() {
-        return firstname() != null
-                && surname() != null
-                && email() != null
+        return firstname != null
                 && !firstname().isBlank()
+                && !firstname.contains(" ")
+                && surname != null
                 && !surname().isBlank()
+                && !surname.contains(" ")
+                && email != null
                 && email().matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
     }
 
